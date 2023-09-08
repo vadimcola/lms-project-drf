b427e7bd287ae6a86199ef8b694ad820fd501418
vadimcola <vadimcola@mail.ru>
2023-09-08 17:13:01 +0700

    Внесены изменение в предстовление

--- learn/views.py
+++ learn/views.py
@@ -10,12 +10,29 @@ from learn.serializers import CourseSerializer, PaymentsSerializer, LessonSerial
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [IsOwner]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(owner=self.request.user.pk)
+        return queryset
+
+    def perform_create(self, serializer):
+        new_course = serializer.save(owner=self.request.user)
+        new_course.owner = self.request.user
+        new_course.save()
 
 
 class LessonList(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [IsOwner]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(owner=self.request.user.pk)
+        return queryset
 
 
 class LessonDetail(generics.RetrieveAPIView):
@@ -28,15 +45,22 @@ class LessonCreate(generics.CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+    def perform_create(self, serializer):
+        new_lesson = serializer.save(owner=self.request.user)
+        new_lesson.owner = self.request.user
+        new_lesson.save()
+
 
 class LessonUpdate(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsOwner]
 
 
 class LessonDelete(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = PaymentsSerializer
+    permission_classes = [IsOwner]
 
 
 class PaymentsList(generics.ListAPIView):
