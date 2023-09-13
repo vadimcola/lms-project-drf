ea49df5ecd1aff4fe30d22a47000b3e83b0ce81c
Vadim Andreev <vadimcola@mail.ru>
2023-09-13 19:58:28 +0700

    Реализован вывод истории платежей по каждому пользователю

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from learn.serializers import PaymentsSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentsSerializer(source='customer', many=True, read_only=True)
+
     class Meta:
         model = User
         fields = '__all__'
