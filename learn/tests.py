6dce477e7d50f1f661bd2144e7c9d12a1f79435c
Vadim Andreev <vadimcola@mail.ru>
2023-09-25 22:23:32 +0700

    Реализованы тесты для модели Уроков

--- learn/tests.py
+++ learn/tests.py
@@ -2,37 +2,25 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
-from learn.models import Course
+from learn.models import Course, Lesson
 from users.models import User
 
 
 class LessonTest(APITestCase):
-    # email = 'test@tast.com'
-    # password = '12345'
 
     def setUp(self):
-        # self.user = User.objects.create(
-        #     email=self.email
-        # )
-        # self.user.set_password(self.password)
-        # self.user.save()
-        #
-        # response = self.client.post(
-        #     '/api/token/',
-        #     {
-        #         'email': self.email,
-        #         'password': self.password
-        #     }
-        # )
-        #
-        # self.token = response.json().get('access')
-        # self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
-
         self.user_data = {'email': 'test@test.com', 'password': '12345'}
         self.user = User.objects.create(**self.user_data)
         self.client.force_authenticate(user=self.user)
 
         self.course = Course.objects.create(course_name='Тест курс')
+
+        self.lesson = Lesson.objects.create(
+            lesson_name='Тест1 имя',
+            lesson_comment='Тест1',
+            lesson_url='https://www.youtube.com/',
+            owner=self.user
+        )
 
     def test_lesson_create(self):
         data_lesson = {
@@ -44,3 +32,22 @@ class LessonTest(APITestCase):
         }
         response = self.client.post(reverse('learn:create'), data_lesson)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_lesson_url_bad(self):
+        data_lesson = {
+            'course_name': self.course.pk,
+            'lesson_name': 'Тест имя',
+            'lesson_comment': 'Тест',
+            'lesson_url': 'https://www.test.com',
+            'owner': self.user.pk
+        }
+        response = self.client.post(reverse('learn:create'), data_lesson)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_lesson_detail(self):
+        response = self.client.get(reverse('learn:detail', args=[self.lesson.pk]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_lesson_delete(self):
+        response = self.client.delete('/lesson/1/delete/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- learn/urls.py
+++ learn/urls.py
@@ -8,10 +8,10 @@ app_name = LearnConfig.name
 
 urlpatterns = [
     path('lesson/', LessonList.as_view(), name='list'),
-    path('lesson/<int:pk>/', LessonDetail.as_view()),
+    path('lesson/<int:pk>/', LessonDetail.as_view(), name='detail'),
     path('lesson/create/', LessonCreate.as_view(), name='create'),
-    path('lesson/<int:pk>/update/', LessonUpdate.as_view()),
-    path('lesson/<int:pk>/delete/', LessonDelete.as_view()),
+    path('lesson/<int:pk>/update/', LessonUpdate.as_view(), name='update'),
+    path('lesson/<int:pk>/delete/', LessonDelete.as_view(), name='delete'),
     path('payments/', PaymentsList.as_view()),
     path('subscribe/', CourseSubscriptionCreate.as_view()),
     path('subscribe/<int:pk>/delete/', CourseSubscriptionDelete.as_view()),
